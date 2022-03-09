9e128d93c16b88aeaf50c7f1fc0e76d78a40ad59
yjjw <yjjw97@gmail.com>
2022-03-09 16:48:04 -0500

    fixed delete test not found

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -361,3 +361,8 @@ class TestYourResourceServer(TestCase):
             content_type = "shopcarts/json"
         )
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_shopcart_not_found(self):
+        """Delete a Shopcart that's not found"""
+        resp = self.app.delete("/shopcarts/0")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
