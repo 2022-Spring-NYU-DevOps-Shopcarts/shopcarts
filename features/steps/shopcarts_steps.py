016e2898efb4dafd6ecd1b6558e4a374f84e2c31
yjjw <yjjw97@gmail.com>
2022-04-28 16:41:58 -0400

    update shopcarts_steps per professor's suggestions

--- features/steps/shopcarts_steps.py
+++ features/steps/shopcarts_steps.py
@@ -16,7 +16,7 @@ def step_impl(context):
     """ Delete all Shopcarts and load new ones """
     headers = {'Content-Type': 'application/json'}
     # list all of the shopcarts and delete them one by one
-    context.resp = requests.get(context.base_url + '/shopcarts', headers=headers)
+    context.resp = requests.get(context.base_url + '/shopcarts')
     expect(context.resp.status_code).to_equal(200)
     for shopcart in context.resp.json():
         context.resp = requests.delete(context.base_url + '/shopcarts/' + str(shopcart["user_id"]), headers=headers)
@@ -30,7 +30,7 @@ def step_impl(context):
             "item_name": row['item_name'],
             "quantity": int(row['quantity']),
             "price": float(row['price']),
-            "hold": bool(row['hold'])
+            "hold": bool(row['hold'] in ['True', 'true', '1'])
         }
         payload = json.dumps(data)
         context.resp = requests.post(create_url+"/"+row['user_id']+"/items", data=payload, headers=headers)
